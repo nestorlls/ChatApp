2ccecafa1851cf460da2e45ef80848fa2b847ee4
Paul Llanque <llanquepol@gmail.com>
2023-06-03 10:48:12 -0500

    add: colors

---
+++ client/src/constants/colors.js
@@ -0,0 +1,24 @@
+export const colors = {
+  bg: [
+    'bg-blue-200',
+    'bg-red-200',
+    'bg-yellow-200',
+    'bg-green-200',
+    'bg-purple-200',
+    'bg-pink-200',
+    'bg-orange-200',
+    'bg-gray-200',
+    'bg-teal-200',
+  ],
+  text: [
+    'text-blue-500',
+    'text-red-500',
+    'text-yellow-500',
+    'text-green-500',
+    'text-purple-500',
+    'text-pink-500',
+    'text-orange-500',
+    'text-gray-500',
+    'text-teal-500',
+  ],
+};
