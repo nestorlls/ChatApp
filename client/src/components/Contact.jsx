23bf949e88769f474d7ca0b05e6f14558f3adce9
Paul Llanque <llanquepol@gmail.com>
2023-06-03 20:14:25 -0500

    add: contact component

---
+++ client/src/components/Contact.jsx
@@ -0,0 +1,20 @@
+import { Avatar } from './Avatar';
+
+export const Contact = ({ userId, username, onClick, selected }) => {
+  return (
+    <div
+      key={userId}
+      className={`flex items-center gap-2 border-gray-600 border-b cursor-pointer ${
+        selected ? 'bg-slate-400' : ''
+      }`}
+      onClick={() => onClick(userId)}>
+      {selected && <div className="w-1 h-12 bg-white rounded-r-lg" />}
+      <div className="flex gap-2 py-3 pl-8 items-center">
+        <Avatar userId={userId} username={username} />
+        <span className={`${selected ? 'text-gray-800' : 'text-white'}`}>
+          {username}
+        </span>
+      </div>
+    </div>
+  );
+};
