3829661b8cb08e76e52ee1a4df533568785bc8d3
Paul Llanque <llanquepol@gmail.com>
2023-06-03 21:40:28 -0500

    update: chat comp user online/offline messages

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -2,18 +2,43 @@ import { useEffect, useState } from 'react';
 import { FormSendMessage } from '../components/FormSendMessage';
 import { Logo } from '../components/Logo';
 import { User } from '../components/User';
+import { Contact } from '../components/Contact';
+import { OnOff } from '../components/OnOff';
+import { useUser } from '../context/UserContext';
+
+const showOnlinePeople = (onlineUsersArr) => {
+  const people = {};
+  onlineUsersArr.forEach(({ userId, username }) => {
+    people[userId] = username;
+  });
+  return people;
+};
 
 export const Chat = () => {
+  const { user } = useUser();
   const [wsConecction, setWsConecction] = useState(null);
+  const [onliPeople, setOnliPeople] = useState({});
+  const [userSelected, setUserSelected] = useState(null);
+  const [messages, setMessages] = useState([]);
 
   useEffect(() => {
     const ws = new WebSocket('ws://localhost:3000');
     setWsConecction(ws);
     ws.addEventListener('message', (event) => {
       const messageData = JSON.parse(event.data);
-      console.log(messageData);
+
+      console.log({ event, messageData });
+      if ('online' in messageData) {
+        const onlinePeople = showOnlinePeople(messageData.online);
+        setOnliPeople(onlinePeople);
+      } else {
+        setMessages((prev) => [...prev, { text: messageData.text }]);
+      }
     });
   }, []);
+
+  const onlinePeopleExclOurUser = { ...onliPeople };
+  delete onlinePeopleExclOurUser[user.userId];
 
   return (
     <div className="flex h-screen">
@@ -23,19 +48,47 @@ export const Chat = () => {
           <User />
         </div>
         <div>
-          <h2>online</h2>
+          <OnOff state="online" />
+          {Object.entries(onlinePeopleExclOurUser).map(([userId, username]) => (
+            <Contact
+              key={userId}
+              userId={userId}
+              username={username}
+              onClick={setUserSelected}
+              selected={userSelected === userId}
+            />
+          ))}
         </div>
         <div>
-          <h2>offline</h2>
+          <OnOff state="offline" />
         </div>
       </aside>
-      <section className="w-3/4 bg-slate-300 flex flex-col p-2">
+      <section className="w-3/4 bg-slate-400 flex flex-col p-2">
         <div className="flex-grow">
-          <p className="text-2xl text-center text-gray-400">
-            &larr;Select a person from the sidebar
-          </p>
+          {!userSelected && (
+            <>
+              <div className="flex justify-center items-center h-full">
+                <p className="text-2xl text-center text-gray-600">
+                  &larr;Select a person from the sidebar
+                </p>
+              </div>
+            </>
+          )}
+          {userSelected && (
+            <>
+              {messages.map((message, i) => (
+                <p key={i}>{message.text}</p>
+              ))}
+            </>
+          )}
         </div>
-        <FormSendMessage />
+        {userSelected && (
+          <FormSendMessage
+            ws={wsConecction}
+            userSelected={userSelected}
+            addMessages={setMessages}
+          />
+        )}
       </section>
     </div>
   );
