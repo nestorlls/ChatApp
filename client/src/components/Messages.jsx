37641c2f55e015d54c41a72bd779fa64f717cbe0
Paul Llanque <llanquepol@gmail.com>
2023-06-05 10:09:36 -0500

    add: messages component

---
+++ client/src/components/Messages.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useRef } from 'react';
+import { useUser } from '../context/UserContext';
+
+export const Messages = ({ sender, text }) => {
+  const { user } = useUser();
+  const divUnderMessages = useRef(null);
+
+  useEffect(() => {
+    const div = divUnderMessages.current;
+    if (div) {
+      div.scrollIntoView({ behavior: 'smooth', block: 'end' });
+    }
+  }, [text]);
+
+  return (
+    <div className={sender === user.userId ? 'text-right' : 'text-left'}>
+      <div
+        className={`text-white inline-block rounded-lg shadow-2xl ${
+          sender === user.userId ? 'bg-blue-600 p-2' : 'bg-gray-500 p-2'
+        }`}>
+        {text}
+      </div>
+      <div ref={divUnderMessages}></div>
+    </div>
+  );
+};
