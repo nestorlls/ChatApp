b96b5a748d0e18a0dd9d841a343252205fbb59f1
Paul Llanque <llanquepol@gmail.com>
2023-06-06 19:50:12 -0500

    update: message component

--- client/src/components/Messages.jsx
+++ client/src/components/Messages.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef } from 'react';
 import { useUser } from '../context/UserContext';
 
-export const Messages = ({ sender, text }) => {
+export const Messages = ({ sender, text, file }) => {
   const { user } = useUser();
   const divUnderMessages = useRef(null);
 
@@ -10,15 +10,35 @@ export const Messages = ({ sender, text }) => {
     if (div) {
       div.scrollIntoView({ behavior: 'smooth', block: 'end' });
     }
-  }, [text]);
+  }, [text, file]);
+
+  const hasFile = (file) => {
+    if (file) {
+      return true;
+    }
+    return false;
+  };
 
   return (
     <div className={sender === user._id ? 'text-right' : 'text-left'}>
       <div
         className={`text-white inline-block rounded-lg shadow-2xl ${
-          sender === user._id ? 'bg-blue-600 p-2' : 'bg-gray-500 p-2'
+          sender === user._id
+            ? hasFile(file) || 'bg-blue-600 p-2'
+            : hasFile(file) || 'bg-gray-500 p-2'
         }`}>
         {text}
+        {file && (
+          <div>
+            <a target="_blank" href={file}>
+              <img
+                className="rounded-lg w-80 object-cover "
+                src={file}
+                alt={file}
+              />
+            </a>
+          </div>
+        )}
       </div>
       <div ref={divUnderMessages}></div>
     </div>
