b5f9fa50276050e3d203aaa4209b23ba5c05339d
Paul Llanque <llanquepol@gmail.com>
2023-06-07 09:52:38 -0500

    add: upload file

--- client/src/components/FormSendMessage.jsx
+++ client/src/components/FormSendMessage.jsx
@@ -9,15 +9,18 @@ export const FormSendMessage = ({ ws, userSelected, addMessages }) => {
     setNewMessage(e.target.value);
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const handleSubmit = async (e, file = null) => {
+    if (e) e.preventDefault();
+
+    ws.send(
+      JSON.stringify({
+        recipient: userSelected,
+        text: newMessage,
+        file,
+      })
+    );
+
     if (newMessage.trim().length > 0) {
-      ws.send(
-        JSON.stringify({
-          recipient: userSelected,
-          text: newMessage,
-        })
-      );
       setNewMessage('');
       addMessages((prev) => [
         ...prev,
@@ -31,8 +34,20 @@ export const FormSendMessage = ({ ws, userSelected, addMessages }) => {
     }
   };
 
+  const uploadFile = (e) => {
+    const reader = new FileReader();
+    const file = e.target.files[0];
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      handleSubmit(null, {
+        name: e.target.files[0].name,
+        data: reader.result,
+      });
+    };
+  };
+
   return (
-    <form className="flex gap-2" onSubmit={handleSubmit}>
+    <form className="flex gap-2 p-2" onSubmit={handleSubmit}>
       <input
         type="text"
         className="input-form"
@@ -40,6 +55,29 @@ export const FormSendMessage = ({ ws, userSelected, addMessages }) => {
         value={newMessage}
         onChange={handleChange}
       />
+      <label
+        htmlFor="file"
+        typeof="button"
+        className="rounded-md bg-gray-200 p-2 text-gray-500 cursor-pointer">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          className="w-6 h-6">
+          <path
+            fillRule="evenodd"
+            d="M18.97 3.659a2.25 2.25 0 00-3.182 0l-10.94 10.94a3.75 3.75 0 105.304 5.303l7.693-7.693a.75.75 0 011.06 1.06l-7.693 7.693a5.25 5.25 0 11-7.424-7.424l10.939-10.94a3.75 3.75 0 115.303 5.304L9.097 18.835l-.008.008-.007.007-.002.002-.003.002A2.25 2.25 0 015.91 15.66l7.81-7.81a.75.75 0 011.061 1.06l-7.81 7.81a.75.75 0 001.054 1.068L18.97 6.84a2.25 2.25 0 000-3.182z"
+            clipRule="evenodd"
+          />
+        </svg>
+        <input
+          type="file"
+          name="file"
+          id="file"
+          className="hidden"
+          onChange={uploadFile}
+        />
+      </label>
       <button type="submit" className="py-2 px-3 bg-blue-500 rounded-md">
         <span className="text-white">
           <svg
